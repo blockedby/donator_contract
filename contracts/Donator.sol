18dc6dc19e465ec05ca5d7e00b60c5e610704b04
blockedby <66838646+blockedby@users.noreply.github.com>
2022-01-09 08:31:08 +0300

    First commit

---
+++ contracts/Donator.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract Donator{
+    address payable public owner;
+    mapping(address=>uint) public contributors;
+    address[] public donaters;
+
+    event NewDonate(address _from, uint _amount);
+    event FullRefund(address _who);
+    event NewTransfer(address _to, uint _amount);
+
+    error Unauthorized();
+    error NotEnoughEther();
+
+    receive() external payable {}
+    fallback() external payable {}
+
+
+    constructor(){
+        owner = payable(msg.sender);
+        contributors[0xccA210e7C05322379F801a07F320E863efE68f80] += 1 ether ;
+    }
+
+    modifier onlyBy(address _account)
+    {
+        if (msg.sender != _account)
+            revert Unauthorized();
+        _;
+    }
+
+    function changeOwner(address _newOwner) public onlyBy(owner){
+        owner = payable(_newOwner);
+    }
+
+    function getBalance() public view returns(uint){
+        return address(this).balance;
+    }
+
+    function donate() public payable {
+        contributors[msg.sender] += msg.value;
+        donaters.push(msg.sender);
+        emit NewDonate(msg.sender,msg.value);
+    }
+
+    function withdraw(address payable _to, uint _amount) public onlyBy(owner){
+        _to.transfer(_amount);
+        emit NewTransfer(_to,_amount);
+    }
+    
+    function getSumOfDonater(address _of) public view returns(uint){
+        return(contributors[_of]);
+    }
+}
---
+++ test/Deploy.js
@@ -0,0 +1,54 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { Contract } = require("hardhat/internal/hardhat-network/stack-traces/model");
+
+describe("Donator contract", function(){
+    let Donator;
+    let donator;
+    let owner;
+    let addr1;
+    let addr2;
+
+    this.beforeEach(async function(){
+        Donator = await ethers.getContractFactory("Donator");
+
+        [owner,addr1,addr2] = await ethers.getSigners();
+
+        donator = await Donator.deploy();
+        await donator.deployed();     
+    })
+    it("Should be successfully deployed", async function (){
+        console.log("success!");
+    })
+    it("Should accepts ether in msg.value", async function(){
+        expect((await donator.getBalance())).to.equal(ethers.BigNumber.from(0));
+        await donator.connect(addr1).donate({
+            value: ethers.utils.parseEther("0.5")
+        });
+        expect((await donator.getBalance())).to.equal(ethers.utils.parseEther("0.5"));
+    })
+    it("Funds can be withdraw to another account by owner", async function(){
+        const transactionHash = await donator.connect(addr1).donate({
+            value: ethers.utils.parseEther("0.5")
+        });
+        await donator.withdraw(owner.address, ethers.utils.parseEther("0.5"));
+        expect((await donator.getBalance())).to.equal(ethers.BigNumber.from(0));
+    })
+    it("Not owner can't do this", async function(){
+        expect(await donator.owner()).to.equal(owner.address);
+        await donator.connect(addr1).donate({
+            value: ethers.utils.parseEther("1.0")
+        });
+        await expect(donator.connect(addr1).withdraw(owner.address, ethers.utils.parseEther("1.0"))).to.be.reverted;
+    })
+    it("Stores contributors addresses and payments", async function() {
+        await donator.connect(owner).donate({
+            value: ethers.utils.parseEther("1.0")
+        });
+        await donator.connect(addr1).donate({
+            value: ethers.utils.parseEther("1.0")
+        });
+        // тут хочу сравнить значение в мапе с тем, что отправил, но не знаю как(((((
+        expect((await donator.donaters[0])).to.equal(owner.address);
+    })
+})
